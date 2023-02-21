a040e0de1afd4ba533bae2c81fb8732679938241
Mohammad Noman <121032599+mohammadnoman07@users.noreply.github.com>
2023-02-21 07:51:29 +0000

    ok

--- pig-game/script.js
+++ pig-game/script.js
@@ -1,1 +1,19 @@
-'use strict';
+let scoreEl =  document.querySelectorAll('.score')
+
+let dice = document.querySelector('.dice')
+let resetEl = document.querySelector('.btn--new')
+
+resetEl.addEventListener('click',function (){
+    scoreEl[0].textContent = 0
+    scoreEl[1].textContent = 0
+    dice.classList.add('hide')
+    
+})
+
+let roll = document.querySelector('.btn--roll')
+roll.addEventListener('click',function (){
+    let randomNumber = Math.floor(Math.random() * 6) + 1
+    dice.classList.remove('hide')
+    dice.src = 'dice-'+randomNumber+'.png'
+
+})
