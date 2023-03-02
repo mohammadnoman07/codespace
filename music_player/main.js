eaf4151a67221b701ea13d5de909f671dea99a88
Mohammad Noman <121032599+mohammadnoman07@users.noreply.github.com>
2023-03-02 07:41:26 +0000

    music palyer javascript

--- music_player/main.js
+++ music_player/main.js
@@ -1,9 +1,6 @@
 let heartIcon = document.querySelector('#heart')
 heartIcon.addEventListener('click', (e) => {
-
-
   e.target.classList.toggle('red')
-   
 })
 let playerTime  = document.querySelector('.player-time-range')
 let currentTime = document.querySelector('.player-time--left')
@@ -11,4 +8,51 @@ playerTime.addEventListener('input', (e) => {
    currentTime.textContent = e.target.value
 })
 
-document.querySelector('.fa-play')
+let audio = document.querySelector("audio");
+
+const song = [
+  "music/stay.mp3",
+  "music/song.mp3",
+  "music/mera-dil.mp3",
+  "music/unstoppable.mp3"
+]
+
+let isPlaying = false;
+let index = 0
+let playBtn = document.querySelector('#playBtn')
+playBtn.addEventListener('click', (e) => {
+  if(isPlaying){
+    e.target.classList.remove('fa-pause')
+    e.target.classList.add('fa-play')
+    audio.pause()
+    isPlaying = false
+
+  }else{
+    audio.src = song[index]
+    e.target.classList.remove('fa-play')
+    e.target.classList.add('fa-pause')
+    audio.play()
+    isPlaying=true
+  }
+})
+let nextBtn = document.querySelector('#nextBtn')
+nextBtn.addEventListener('click',(e) =>{
+  index++
+  if(index == song.length-1){
+    index = 0
+  }
+  audio.src = song[index]
+  audio.play()
+  isPlaying = true
+})
+let prevBtn = document.querySelector('#prevBtn')
+prevBtn.addEventListener('click',(e)=>{
+  
+  index--
+  if(index <= 0 ){
+    index = song.length-1
+  }
+  audio.src= song[index]
+  audio.play()
+  isPlaying = true
+})
